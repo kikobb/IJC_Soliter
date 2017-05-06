a17c69bc75633bd4c1b0c59c22054dd49f53e08d
Mato <uhliarik1@azet.sk>
2017-05-06 13:26:45 +0200

    Added TargetStack body

--- model/TargetStack.cpp
+++ model/TargetStack.cpp
@@ -3,3 +3,23 @@
 //
 
 #include "TargetStack.h"
+
+bool TargetStack::put(Card *card) {
+
+    if (this->isEmpty()){
+        if (card->color() == packColor) {
+            if (card->value() == 1) {
+                this->put(card);
+                return true;
+            }
+        }
+    }
+    else
+    if ((card->value() - this->get()->value()) == 1) {
+        if (card->color() == packColor) {
+            this->put(card);
+            return true;
+        }
+    }
+    return false;
+}
